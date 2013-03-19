edeeecb2b685276d45b70f0ec0ce15c03e375649
zhanghao <jack_rab@126.com>
2013-03-19 09:05:04 +0800

    Signed-off-by: zhanghao <jack_rab@126.com>

--- src/com/test/mina/Client.java
+++ src/com/test/mina/Client.java
@@ -1,6 +1,7 @@
 package com.test.mina;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
