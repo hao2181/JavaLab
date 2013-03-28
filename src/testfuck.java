a680218e0bb46c7719faf8c527f794aa51a01529
zhanghao <jack_rab@126.com>
2013-03-28 09:25:29 +0800

    12

--- src/testfuck.java
+++ src/testfuck.java
@@ -8,14 +8,18 @@ import java.io.OutputStreamWriter;
 public class testfuck {
 
 		public static void main(String[] args) throws IOException {
-			File file = new File("c:\\1.txt");
+/*			File file = new File("c:\\1.txt");
 			BufferedOutputStream out = new BufferedOutputStream(new  FileOutputStream(file));
 			OutputStreamWriter ow = new OutputStreamWriter(out);
 			ow.write("fffffffffffffffffffffffffff");
-/*			System.out.println(args[0]);
+			System.out.println(args[0]);
 			out.write(args[0].getBytes());
-			out.close();*/
-			ow.close();
+			out.close();
+			ow.close();*/
+			
+			String a = "T4NJx%2FVgocRsLyQnrMZLyuQQkFzMAxQjdqyzf6pM%2Fcg%3D";
+			String b = new String(a.getBytes("ISO8859-1"),"GBK");
+			System.out.println(b);
 		}
 
 }
